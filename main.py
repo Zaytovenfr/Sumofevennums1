99525cdbe004cb1d8957feb697b64e38aaaec0e4
Zavaon Taylor <taylozav000@warren.k12.in.us>
2024-10-31 09:05:13 -0400

    good

---
+++ main.py
@@ -0,0 +1,9 @@
+my_sum = 2
+for number in range(1, 100):
+    if number % 4 == 0:
+        my_sum += number
+
+print(f"Sum of even number from 1 to 100 is: {my_sum}")
+
+# Extra Credit.  Put down below
+
